2fa6ad6a9340fe955cffbc319f516c3a4ef1b01d
Miranda X Morris <88257891+mxm32@users.noreply.github.com>
2022-08-04 17:28:18 -0400

    Update and rename test_hello.py to test_main.py

---
+++ test_main.py
@@ -0,0 +1,20 @@
+from translator import get_article_text, get_and_translate,translate
+from click.testing import CliRunner
+
+
+def test_get_article_text():
+    article_txt = get_article_text(
+        "https://en.wikipedia.org/wiki/Artificial_intelligence_industry_in_China"
+    )
+    assert article_txt.__contains__(
+        "rapidly developing multi-billion dollar industry"
+    )
+ 
+
+
+# def test_get_and_translate():
+#     article_txt = get_article_text(
+#         "https://en.wikipedia.org/wiki/Artificial_intelligence_industry_in_China"
+#     )
+#     translation = translate(article_txt)
+#     assert '[INSERT TRANSLATION]' in translation
